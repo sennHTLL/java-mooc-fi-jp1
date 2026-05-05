372f353e07d58a774fbfe26a6e6102193635f69b
sennHTLL <senthecode@gmail.com>
2026-05-05 16:49:47 +0500

    [session · 1]

--- P3/app/src/main/java/jpi/p3/Exercise14.java
+++ P3/app/src/main/java/jpi/p3/Exercise14.java
@@ -9,6 +9,7 @@ public class Exercise14 {
 
         IO.println("[array] · user creates array");
         int[] array = uCreatesArray(scanner);
+
         IO.println("Here are the array:");
         for (int i = 0; i < array.length; i++) {
             IO.println(array[i]);
@@ -18,9 +19,10 @@ public class Exercise14 {
     public static int[] uCreatesArray(Scanner scanner){
         IO.print("Enter size of array: ");
         int uArraySize = Integer.valueOf(scanner.nextLine());
+
         int[] uArray = new int[uArraySize];
-        IO.println("Enter elements of array - numbers:");
         
+        IO.println("Enter elements of array - numbers:");
         for(int i = 0; i < uArray.length; i++){
             int uElements = Integer.valueOf(scanner.nextLine());
             uArray[i] = uElements;
